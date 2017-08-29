d780be35bd0fab8a9739251628c2b22cbe05caf8
connordelacruz <connor.c.delacruz@gmail.com>
2017-08-29 18:34:08 -0500

    Renamed install scripts and created stub for setting up nvim to work with this configuration

--- install_extended.sh
+++ install_extended.sh
@@ -3,6 +3,7 @@ set -e
 
 cd ~/.vim_runtime
 
+# TODO: backup existing vimrcs
 echo 'set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/basic.vim
@@ -15,4 +16,4 @@ source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+echo "Installed extended vim configuration."
---
+++ install_nvim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: make ~/.config/nvim/ if it doesn't already exist
+
+# TODO: backup existing init.vim
+
+# TODO: source ~/.vimrc
+
+# TODO: echo results
