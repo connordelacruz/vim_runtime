8bf7f6a97dcec21018fe536d1460ae4f7e44cbb2
Connor de la Cruz <cdelacruz@sccadv.com>
2019-10-16 10:32:01 -0500

    Added single entry point to be sourced in .vimrc
    - Added init.vim, which handles sourcing all the individual scripts
    - Updated install_extended.sh to only source init.vim

--- install/install_extended.sh
+++ install/install_extended.sh
@@ -23,17 +23,7 @@ fi
 
 # create new .vimrc
 echo 'Creating new .vimrc...'
-echo 'set runtimepath+=~/.vim_runtime
-
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
-
-try
-source ~/.vim_runtime/my_configs.vim
-catch
-endtry' > ~/.vimrc
+echo 'source ~/.vim_runtime/init.vim' > ~/.vimrc
 echo 'New .vimrc created.'
 
 # Initialize submodules
